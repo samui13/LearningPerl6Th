e899c126feeb8d333e3b9b4bcd9a50eb17dd53a7
samui13 <ss.to13@gmail.com>
2014-05-26 07:12:40 +0900

    Read 2.2.2

--- 2.2.pl
+++ 2.2.pl
@@ -5,4 +5,12 @@ use strict;
 print 'fred'."\n";
 print 'This is\n\n'."\n";
 print '\''."\n";
+# 2.2.2
+print "Cake\tPHP\n";
+print "\lWWWWW\n";
+print "\uwwwww\n";
+
+print "\LWWW\EWW\n";
+print "\Uwww\Eww\n";
+print "\Qwwww\n"."\n";
 
