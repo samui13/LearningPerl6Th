722db6cf9ce40b5f73582c8d1dac5425d49dd152
samui13 <ss.to13@gmail.com>
2014-05-26 06:49:24 +0900

    Read 2.1.4 Decimal Number

--- practice.pl
+++ practice.pl
@@ -2,4 +2,8 @@
 print 1.25e45."\n";
 print (10+1)."\n";
 print "\n";
-print 100_210 + 100_210;
+print 100_210 + 100_210 ."\n";
+# 2.1.3
+print 0377."\n";
+print 0xff."\n";
+print 0b11_11_11_11."\n";
