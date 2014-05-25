0436f9aadefd0869571df76cf4c636c5cb0519b8
samui13 <ss.to13@gmail.com>
2014-05-26 06:54:41 +0900

    Read 2.1.5

--- 2.1.pl
+++ 2.1.pl
@@ -3,7 +3,13 @@ print 1.25e45."\n";
 print (10+1)."\n";
 print "\n";
 print 100_210 + 100_210 ."\n";
-# 2.1.3
+# 2.1.4
 print 0377."\n";
 print 0xff."\n";
 print 0b11_11_11_11."\n";
+# 2.1.5
+print 1+1 ."\n";
+print 10.2/0.3 ."\n";
+print (10/3*3+0.3) ."\n";
+print "\n";
+print 2**3 ."\n";
