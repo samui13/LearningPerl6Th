9701925146a3c5358e289815e0fa3bbea6c5df8b
samui13 <ss.to13@gmail.com>
2014-05-26 07:37:44 +0900

    Read 2.4.2

--- 2.4.pl
+++ 2.4.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 # -*- coding: utf-8; -*-
-# Last-Updated : <2014/05/26 07:31:51 by samui>
+# Last-Updated : <2014/05/26 07:37:04 by samui>
 
 use strict;
 use warnings;
@@ -8,4 +8,11 @@ use utf8;
 # 2.4
 my $name = 'ABC';
 print $name."\n";
+
+# 2.4.2 
+my $fred = 17;
+my $barney = 'hello';
+$barney = $barney x 3;
+print $barney."\n";
+
 
