f75c5cf4454499b03328a52d36a90bca373f29f2
samui13 <ss.to13@gmail.com>
2014-05-26 11:04:03 +0900

    Read 4.1

---
+++ 4.1.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+# -*- coding: utf-8; -*-
+# Last-Updated : <2014/05/26 11:03:28 by samui>
+
+use strict;
+use warnings;
+use utf8;
+
+sub marine {
+    my $n += 1;
+    print "Hello, sailor number $n!\n";
+}
+&marine;
+&marine;
