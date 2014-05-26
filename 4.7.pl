bcef2e6b8f8aee3be517736dcbcf1b3f02e65baf
samui13 <ss.to13@gmail.com>
2014-05-26 12:30:45 +0900

    Read 4.7

---
+++ 4.7.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+# -*- coding: utf-8; -*-
+# Last-Updated : <2014/05/26 12:27:45 by samui>
+
+use strict;
+use warnings;
+use utf8;
+
+my @array = (1..10);
+my ($first) = @array;
+my $len = @array;
+print @array;
+print "\n";
+print $first;
+print "\n";
+print $len;
+print "\n";
