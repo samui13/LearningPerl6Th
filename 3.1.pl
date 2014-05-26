d3d13873470c21107017f9e52725bfc7eb599c67
samui13 <ss.to13@gmail.com>
2014-05-26 09:07:23 +0900

    Add Section 3

---
+++ 3.1.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+# -*- coding: utf-8; -*-
+# Last-Updated : <2014/05/26 08:34:10 by samui>
+
+use strict;
+use warnings;
+use utf8;
+
+# 3.1.1
+my @fred;
+$fred[0] = "Yabbba";
+$fred[1] = "Yabbba1";
+print $fred[0];
+print $fred[1];
